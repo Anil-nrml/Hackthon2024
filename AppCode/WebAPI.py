1d2d1c6b431ebd6c017ca013451dc50651195240
santoshparida000 <77623649+santoshparida000@users.noreply.github.com>
2024-03-17 13:51:15 +0530

    Update WebAPI.py

--- AppCode/WebAPI.py
+++ AppCode/WebAPI.py
@@ -1,4 +1,5 @@
 class WebAPIURL:
     def GetWebAPIURL(methhod):
-        WebAPI_URL : str = 'https://vaibhav84-resumeapi.hf.space/'
+        #WebAPI_URL : str = 'https://vaibhav84-resumeapi.hf.space/'
+        WebAPI_URL : str = 'https://vaibhav84-hackerspaceapi.hf.space/'
         return    WebAPI_URL + methhod 
